0ba53a4396bfc35e0babd24939d1f952ea14b770
Dave <8709533+DaveBathnes@users.noreply.github.com>
2022-06-01 13:43:35 +0100

    Adding maplibre and initial base map (#15)

---
+++ src/MapUpMap.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Map from 'react-map-gl';
+import maplibregl from 'maplibre-gl';
+
+function MapUpMap() {
+  return (
+    <Map
+      initialViewState={{
+        latitude: 50.9382,
+        longitude: -1.471061,
+        zoom: 16
+      }}
+      mapLib={maplibregl}
+      style={{ width: '100vw', height: '100vh' }}
+      mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+    />
+  );
+}
+
+export default MapUpMap;
