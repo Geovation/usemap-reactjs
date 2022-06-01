44039f3529e857b5bfa1e05284762b3a6c392777
Dave <8709533+DaveBathnes@users.noreply.github.com>
2022-06-01 13:42:56 +0100

    Adding maplibre and initial base map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 
+import MapUpMap from './MapUpMap';
+
 import './App.css';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
+      <MapUpMap />
     </React.Fragment>
   );
 }
