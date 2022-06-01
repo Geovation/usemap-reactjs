65d5580922707bcd4992cb7bc6da183e55afe056
Dave <8709533+DaveBathnes@users.noreply.github.com>
2022-06-01 15:23:50 +0100

    Adding work for deploy on gh pages

--- src/MapUpMap.js
+++ src/MapUpMap.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Map from 'react-map-gl';
-import maplibregl from 'maplibre-gl';
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import maplibregl from "!maplibre-gl";
 
 function MapUpMap() {
   return (
