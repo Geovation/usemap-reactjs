a0ddf9b3efdca3c5c3fa8b8f5c32e2eb4cc1bc73
Dave <8709533+DaveBathnes@users.noreply.github.com>
2022-06-07 14:41:59 +0100

    Adding inactive search component (#18)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+
+import AppBar from '@mui/material/AppBar';
 import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+
+import AdbIcon from '@mui/icons-material/Adb';
 
 import MapUpMap from './MapUpMap';
+import Search from './Search';
 
 import './App.css';
 import 'maplibre-gl/dist/maplibre-gl.css';
@@ -10,6 +16,12 @@ function App() {
   return (
     <React.Fragment>
       <CssBaseline />
+      <AppBar position="fixed" color="transparent" elevation={0}>
+        <Toolbar>
+          <Search />
+          <AdbIcon sx={{ mr: 1 }} />
+        </Toolbar>
+      </AppBar>
       <MapUpMap />
     </React.Fragment>
   );
