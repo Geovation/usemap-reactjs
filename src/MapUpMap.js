a12722babfd3670c7f16e885cac5cdd561a6b61d
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2022-09-22 16:44:36 +0100

    reset mapupmap.js

--- src/MapUpMap.js
+++ src/MapUpMap.js
@@ -3,40 +3,19 @@ import Map from 'react-map-gl';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import maplibregl from "!maplibre-gl";
-import axios from "axios";
 
 function MapUpMap() {
-
-  let apiKey = "my-api-key";
-  let serviceUrl = 'https://api.os.uk/maps/vector/v1/vts';
-
-  // BRANDON TEST AXIOS
-  async function find() {
-      axios.get('https://api.os.uk/search/names/v1/find?query=Southampton&key=' + apiKey)
-      .then(function (response) {
-          /* For explanation and debugging purposes we display the full response from the API in the console */
-          console.log(JSON.stringify(response.data, null, 2));
-      });
-  }
-  find()
-  //
-
   return (
     <Map
       initialViewState={{
         latitude: 50.9382,
         longitude: -1.471061,
-        zoom: 2
+        zoom: 16
       }}
       mapLib={maplibregl}
       style={{ width: '100vw', height: '100vh' }}
-      //mapStyle="OS_3857_Light_edited.json"
-      //two other ways to get styles: either through an API call or from github raw. take your pick.
-      mapStyle={serviceUrl + "/resources/styles?srs=3857&key=" + apiKey}
-      //mapStyle="https://raw.githubusercontent.com/OrdnanceSurvey/OS-Vector-Tile-API-Stylesheets/master/OS_VTS_3857_Light.json"
+      mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
     />
-
-    
   );
 }
 
