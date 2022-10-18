c4a0a15e2d6da01e3fea212fee0329a42a406a97
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2022-10-18 13:51:14 +0100

    fix loading issue

--- src/hooks/usePlaces.js
+++ src/hooks/usePlaces.js
@@ -2,11 +2,12 @@ import { PlacesAPI } from '../api/placesAPI'
 import { useState } from 'react'
 
 const usePlaces = () => {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [places, setPlaces] = useState([])
 
   const searchPlaces = (search) => {
     PlacesAPI.autofill(search).then((response) => {
+      setLoading(true)
       return response
     })
       .then((response) => {
